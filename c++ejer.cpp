93020b1e3a448a5e595c74bd6d0098fabb4f10ad
mateotr240 <mateotr24033@gmail.com>
2025-02-13 08:08:55 -0500

    Create c++ejer.cpp

---
+++ c++ejer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int x=100;
+    int y=2;
+    int l=15;
+    int w=400;
+    int p=(w-(w*l/100));
+    int r=10;
+    std::cout <<(x+y)<<std::endl;
+    std::cout <<(x-y)<<std::endl;
+    std::cout <<(x*y)<<std::endl;
+    std::cout <<(x/y)<<std::endl;
+    std::cout <<(x*y/100)<<std::endl;
+    std::cout <<(p)<<std::endl;
+    std::cout <<((r+5-3)*2/4)<<std::endl;
+    
+    return 0;
+}
